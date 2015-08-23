e5a8af6d7048d88b63d0694dd67d14a3e3144377
William-Bachman <william_bachman@outlook.com>
2015-08-23 14:18:13 -0400

    Initial Commit

---
+++ CSharpOnlineMVC.Model/Movie.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpOnlineMVC.Model
+{
+    public class Movie
+    {
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+        public int YearReleased { get; set; }
+        public string ImageUrl { get; set; }
+        public MovieRating Rating { get; set; }
+
+        public Movie()
+        {
+        }
+
+        public Movie(int id, string title, int year, string image, MovieRating rating)
+        {
+            this.MovieId = id;
+            this.Title = title;
+            this.YearReleased = year;
+            this.ImageUrl = image;
+            this.Rating = rating;
+        }
+    }
+
+    public enum MovieRating
+    {
+        G,
+        PG,
+        PG13,
+        R
+    }
+}
---
+++ CSharpOnlineMVC/Models/DB.cs
@@ -0,0 +1,33 @@
+﻿using CSharpOnlineMVC.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CSharpOnlineMVC.Models
+{
+    public static class DB
+    {
+        private static List<Movie> _movies { get; set; }
+        public static List<Movie> Movies
+        {
+            get
+            {
+                if (_movies == null)
+                {
+                    _movies = new List<Movie>()
+                    {
+                        new Movie(1, "Return of the Jar Jar", 2000, "http://img1.wikia.nocookie.net/__cb20111208042120/starwars/images/4/42/JarJarHS-SWE.jpg", MovieRating.PG),
+                        new Movie(2, "Old Yeller", 1985, "http://www.oldyelladogranch.com/puppies.jpg", MovieRating.R),
+                        new Movie(3, "Game of Thrones, the Musical!", 2016, "http://i.guim.co.uk/static/w-620/h--/q-95/sys-images/Guardian/Pix/pictures/2014/6/27/1403860543867/66da8667-b19f-4eda-af46-c8adb01211d7-1020x612.jpeg", MovieRating.G)
+                    };
+                }
+                return _movies;
+            }
+            set
+            {
+                _movies = value;
+            }
+        }
+    }
+}
